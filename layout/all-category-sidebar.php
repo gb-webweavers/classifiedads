5d97699c80d5d626c0756456cd1d20dfa6b8635c
gb-web <80595400+gb-webweavers@users.noreply.github.com>
2021-08-31 02:04:38 +0530

    assets/

---
+++ layout/all-category-sidebar.php
@@ -0,0 +1,67 @@
+<div class="main-container section-padding">
+   <div class="container">
+      <div class="row">
+
+<div class="col-lg-3 col-md-12 col-xs-12 page-sidebar">
+   <aside>
+      <div class="widget_search">
+         <form role="search" id="search-form">
+            <input type="search" class="form-control" autocomplete="off" name="s" placeholder="Search..." id="search-input" value="">
+            <button type="submit" id="search-submit" class="search-btn"><i class="lni-search"></i></button>
+         </form>
+      </div>
+      <div class="widget categories">
+         <h4 class="widget-title">All Categories</h4>
+         <ul class="categories-list">
+            <li>
+               <a href="#">
+               <i class="lni-dinner"></i>
+               Hotel & Travels <span class="category-counter">(5)</span>
+               </a>
+            </li>
+            <li>
+               <a href="#">
+               <i class="lni-control-panel"></i>
+               Services <span class="category-counter">(8)</span>
+               </a>
+            </li>
+            <li>
+               <a href="#">
+               <i class="lni-github"></i>
+               Pets <span class="category-counter">(2)</span>
+               </a>
+            </li>
+            <li>
+               <a href="#">
+               <i class="lni-coffee-cup"></i>
+               Restaurants <span class="category-counter">(3)</span>
+               </a>
+            </li>
+            <li>
+               <a href="#">
+               <i class="lni-home"></i>
+               Real Estate <span class="category-counter">(4)</span>
+               </a>
+            </li>
+            <li>
+               <a href="#">
+               <i class="lni-pencil"></i>
+               Jobs <span class="category-counter">(5)</span>
+               </a>
+            </li>
+            <li>
+               <a href="#">
+               <i class="lni-display"></i>
+               Electronics <span class="category-counter">(9)</span>
+               </a>
+            </li>
+         </ul>
+      </div>
+      <div class="widget">
+         <h4 class="widget-title">Advertisement</h4>
+         <div class="add-box">
+            <img class="img-fluid" src="assets/img/img1.jpg" alt="">
+         </div>
+      </div>
+   </aside>
+</div>
---
+++ layout/main-search-area.php
@@ -0,0 +1,34 @@
+ <div class="search-bar">
+    <div class="search-inner">
+        <form class="search-form">
+            <div class="form-group">
+                <input type="text" name="customword" class="form-control" placeholder="What are you looking for?">
+            </div>
+            <div class="form-group inputwithicon">
+                <div class="select">
+                    <select>
+                        <option value="none">Select Catagory</option>
+                        <option value="none">Jobs</option>
+                        <option value="none">Electronics</option>
+                        <option value="none">Mobile</option>
+                        <option value="none">Training</option>
+                        <option value="none">Pets</option>
+                        <option value="none">Real Estate</option>
+                        <option value="none">Services</option>
+                        <option value="none">Training</option>
+                        <option value="none">Vehicles</option>
+                    </select>
+                </div>
+                <i class="lni-menu"></i>
+            </div>
+            <button class="btn btn-common" type="button"><i class="lni-search"></i> Search Now</button>
+        </form>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</header>
